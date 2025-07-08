cd896edb728627bc55a818a770f5eb8fa5250ed8
Alejandro Mazzitelli <alejandro@mazzitelli.com.ar>
2025-07-08 14:46:40 -0300

    Comando de Instalacion, conexion y desconexion a Microsoft Teams.

--- Borrador MSTeamsV1 .ps1
+++ Borrador MSTeamsV1 .ps1
@@ -1,2 +1,9 @@
+#Instalacion del Modulo de Microsoft Teams
 Install-Module -Name MicrosoftTeams -Force -AllowClobber
+
+#Conectarse a Microsoft Teams
+Connect-MicrosoftTeams
+
+#Desconectarse de Microsoft Teams
+Disconnect-MicrosoftTeams
 
