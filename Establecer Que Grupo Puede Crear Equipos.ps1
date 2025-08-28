307d794f05345beef1142494f56842385e697129
Alejandro Mazzitelli <alejandro@mazzitelli.com.ar>
2025-08-27 22:18:45 -0300

    Leves ajustes al script para corregir un error tipográfico en el nombre del grupo.

--- Establecer Que Grupo Puede Crear Equipos.ps1
+++ Establecer Que Grupo Puede Crear Equipos.ps1
@@ -1,16 +1,28 @@
-#Instalar (si hace falta) y conectarte
-Install-Module Microsoft.Graph -Scope CurrentUser -AllowClobber
-Install-Module Microsoft.Graph.Beta.Identity.DirectoryManagement
-Install-Module Microsoft.Graph.Beta.Groups
-Import-Module Microsoft.Graph
-Import-Module Microsoft.Graph.Beta.Identity.DirectoryManagement
-Import-Module Microsoft.Graph.Beta.Groups
+#--------------------------------------------------------
+# Script: Establecer Que Grupo Puede Crear Equipos.ps1
+# Author: Alejandro Mazzitelli
+# Email: alejandro@mazzitelli.com.ar
+# Web: https://www.mazzitelli.com.ar
+# Description: En Este script se puede establecer que grupo de usuarios puede crear equipos en Microsoft Teams. Adicionalmente, se puede deshabilitar la creación de equipos para todos los usuarios.
+#              Para que un grupo pueda crear equipos, primero debe existir el grupo en AzureAD.	Tambien se puede consultar que grupo tiene permisos para crear equipos.
+#              El script utiliza el módulo Microsoft.Graph.Beta, por lo que es posible que se requiera instalarlo. Al inicio del script se realiza la instalación si es necesario.
+# Version: 1.0
+# LastUpDate: 2025-08-17
+#--------------------------------------------------------
+
+#Instalar (si hace falta) y cargar los módulos necesarios
+#Install-Module Microsoft.Graph -Scope CurrentUser -AllowClobber
+#Install-Module Microsoft.Graph.Beta.Identity.DirectoryManagement
+#Install-Module Microsoft.Graph.Beta.Groups
+#Import-Module Microsoft.Graph
+#Import-Module Microsoft.Graph.Beta.Identity.DirectoryManagement
+#Import-Module Microsoft.Graph.Beta.Groups
 
 # Conectarse con permisos
 Connect-MgGraph -Scopes "Directory.ReadWrite.All", "Group.Read.All"
 
 #Configurar el grupo que puede crear equipos
-$GroupName = "SalesAgents"
+$GroupName = "SG-CrearEquipos"
 $AllowGroupCreation = "False"
 
 #Establecer el grupo que puede crear equipos
